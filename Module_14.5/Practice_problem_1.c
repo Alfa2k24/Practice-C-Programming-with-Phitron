5730b8326549e3fda1a88f77686330ab68c5e872
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-04-30 23:07:11 +0600

    practice..

--- Module_14.5/Practice_problem_1.c
+++ Module_14.5/Practice_problem_1.c
@@ -6,7 +6,7 @@ int main(){
     int star = 1;
     for(int i = 1; i <= num; i++){
         for(int j = 1; j <= space; j++){
-            printf(" ");
+            printf(".");
         } 
         space--;
 
